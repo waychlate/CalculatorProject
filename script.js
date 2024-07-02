4ff8d8738eb1ead140b8e4101c9e8cc4711f892f
leyleinr <46614585+waychlate@users.noreply.github.com>
2024-07-02 18:49:27 -0400

    Implement negative/positive sign

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const contentContainer = document.querySelector(".calculator-container");
 const display = document.querySelector(".display-container p");
 
 const possibleOperators = "-+÷×";
-const specialOperators = "=c";
+const specialOperators = "=c+/-";
 
 let firstNum = [];
 let secondNum = [];
@@ -30,19 +30,29 @@ let operator = '';
 let sum = 0;
 
 function eval (first, second, opp) {
-    sum = parseFloat(operate(parseFloat(firstNum.join('')), parseFloat(secondNum.join('')), operator).toFixed(3));
+    sum = parseFloat(operate(parseFloat(firstNum.join('')), parseFloat(secondNum.join('')), operator).toFixed(12));
     
     firstNum = [];
-    firstNum.push(sum);
+    for (let v of sum.toString().split('')) {
+        firstNum.push(v)
+    }
     secondNum = [];
     display.textContent = firstNum.join('');
+    console.log(firstNum)
+
+    if (sum == 'Infinity') {
+        display.textContent = 'bruh';
+        firstNum = [];
+        secondNum = [];
+        operator = '';
+        sum = 0;
+    }
 }
 
 contentContainer.addEventListener("click", (event) => {
     let char = event.target.textContent;
 
     if (event.target.nodeName == "BUTTON") {
-        //In the future integrate the negative sign 
         if (firstNum.length == 0 && possibleOperators.includes(char)) return; 
 
         if (firstNum.length != 0  && secondNum.length == 0 && possibleOperators.includes(char)) {
@@ -72,21 +82,53 @@ contentContainer.addEventListener("click", (event) => {
                     sum = 0;
                     display.textContent = '0';
                     return;
+                case '+/-':
+                    if (display.textContent == firstNum.join('') && !firstNum.includes('-')) {
+                        firstNum.unshift('-');
+                        display.textContent = firstNum.join('');
+                    } else if (display.textContent == firstNum.join('') && firstNum.includes('-')) {
+                        firstNum.shift();
+                        display.textContent = firstNum.join('');
+                    }
+                    if (display.textContent == secondNum.join('') && !secondNum.includes('-')) {
+                        secondNum.unshift('-');
+                        display.textContent = secondNum.join('');
+                    } else if (display.textContent == secondNum.join('') && secondNum.includes('-')) {
+                        secondNum.shift();
+                        display.textContent = secondNum.join('');
+                    }
+                    return;
             }
         }
 
         //Get a firstNum value
         if (!operator && !possibleOperators.includes(char) && !specialOperators.includes(char)) {
-            firstNum.push(char);
-            display.textContent = firstNum.join('');
-            return;
+            if (!firstNum.includes('.') && char == '.') {
+                firstNum.push(char);
+                display.textContent = firstNum.join('');
+                return;
+            } else if (firstNum.includes('.') && char == '.'){
+                return;
+            } else {
+                firstNum.push(char);
+                display.textContent = firstNum.join('');
+                return;
+            }
         }
 
         //Get a secondNum value
         if (operator && !possibleOperators.includes(char) && !specialOperators.includes(char)) {
-            secondNum.push(char);
-            display.textContent = secondNum.join('');
-            return;
+            if (!secondNum.includes('.') && char == '.') {
+                secondNum.push(char);
+                display.textContent = secondNum.join('');
+                return;
+            } else if (secondNum.includes('.') && char == '.'){
+                return;
+            } else {
+                secondNum.push(char);
+                display.textContent = secondNum.join('');
+                return;
+            }
         }
     }
 })
