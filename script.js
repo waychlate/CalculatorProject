ef01cfe852afe6a36b355130e7d2ac72d209d1c9
leyleinr <46614585+waychlate@users.noreply.github.com>
2024-07-02 18:02:10 -0400

    Implement basic calcualtor functions and display integration

--- script.js
+++ script.js
@@ -3,23 +3,85 @@ let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
 
-let numOne = 0;
-let numTwo = 0;
-let operation = '';
-
 function operate (firstNum, secondNum, operator) {
-    switch(operator) {
+    switch (operator) {
         case '+':
-            add(firstNum, secondNum);
-            break;
+            return add(firstNum, secondNum);
         case '-':
-            subtract(firstNum, secondNum);
-            break;
-        case 'x':
-            multiply(firstNum, secondNum);
-            break;
-        case '/':
-            divide(firstNum, secondNum);
-            break;
+            return subtract(firstNum, secondNum);
+        case '×':
+            return multiply(firstNum, secondNum);
+        case '÷':
+            return divide(firstNum, secondNum);
+        default:
+            return '';
     }
 }
+
+const contentContainer = document.querySelector(".calculator-container");
+const display = document.querySelector(".display-container p");
+
+const possibleOperators = "-+÷×";
+const specialOperators = "=c";
+
+let firstNum = [];
+let secondNum = [];
+let operator = '';
+let sum = 0;
+
+function eval (first, second, opp) {
+    sum = operate(parseInt(firstNum.join('')), parseInt(secondNum.join('')), operator);
+    console.log(sum)
+    
+    firstNum = [];
+    firstNum.push(sum);
+    secondNum = [];
+    display.textContent = firstNum.join('');
+}
+
+contentContainer.addEventListener("click", (event) => {
+    let char = event.target.textContent;
+
+    if (event.target.nodeName == "BUTTON") {
+        //In the future integrate the negative sign 
+        if (firstNum.length == 0 && possibleOperators.includes(char)) return; 
+
+        if (firstNum.length != 0  && possibleOperators.includes(char)) {
+            if (firstNum.length != 0 && secondNum.length != 0 && operator) {
+                eval(firstNum, secondNum, operator);
+            } else {
+                operator = char;
+            }
+        }
+
+        if (specialOperators.includes(char)) {
+            switch(char) {
+                case '=':
+                    if (firstNum.length != 0 && secondNum.length != 0 && operator) eval(firstNum, secondNum, operator);
+                    return;
+                case 'c':
+                    firstNum = [];
+                    secondNum = [];
+                    operator = '';
+                    sum = 0;
+                    display.textContent = '0';
+                    return;
+            }
+        }
+
+        //Get a firstNum value
+        if (!operator && !possibleOperators.includes(char) && !specialOperators.includes(char)) {
+            firstNum.push(char);
+            display.textContent = firstNum.join('');
+            return;
+        }
+
+        //Get a secondNum value
+        if (operator && !possibleOperators.includes(char) && !specialOperators.includes(char)) {
+            secondNum.push(char);
+            display.textContent = secondNum.join('');
+            return;
+        }
+    }
+})
+
